37b618a43a3ea2b106f01b27d88f9473285511ad
BaileyJames <baileyheyworth@gmail.com>
2021-08-16 17:46:49 +0100

    implemented a working search engine

--- frontend/src/routes/products/search.svelte
+++ frontend/src/routes/products/search.svelte
@@ -1,10 +1,31 @@
 <script>
     import { page } from '$app/stores';
 
+    console.log($page.query.get("product"))
+    const query = $page.query.get("product")
+    let myData = []
 
-const {slug} = $page.params
-console.log($page.query.get("product"))
-
+    const fetchData = fetch(`http://localhost:4000/api/db/${query}`).then((response) => response.json()).then((data) => {
+        myData = data
+        console.log(data)
+    });
+    
 </script>
 
-hello
+{#await fetchData}
+    <p>Loading results...</p>
+    {:then}
+        {#each myData as product}
+        <div class="product">
+            <p>{product.product}</p>
+            <p>£{product.price}</p>
+        </div>
+        
+        {/each}
+{/await}
+
+<style>
+    .product {
+        background-color: grey;
+    }
+</style>
