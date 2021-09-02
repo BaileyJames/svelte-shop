6ac90afc029c7ab4fcb595f02ac4a2fe771e3559
BaileyJames <baileyheyworth@gmail.com>
2021-09-02 15:14:42 +0100

    completed a submit form to add a new product.

--- frontend/src/routes/admin/products/addProduct.svelte
+++ frontend/src/routes/admin/products/addProduct.svelte
@@ -1,5 +1,11 @@
 <script>
-    let hi = "hi";
+    let largestId = "hi";
+    const fetchData = fetch("http://localhost:4000/api/db").then((response) => response.json()).then((data) => {
+        //Gets the largest id in the database, and adds one to it. This is so the database
+        //automatically has an inputted unique id
+        console.log(data[0]._id)
+        largestId = data[0]._id + 1
+    });
 </script>
 <div class="product-wrapper">
     <div class="product">
@@ -12,10 +18,14 @@
             <br>
             <input type="date" id="expiration date" name="expiration date" placeholder="Expiry date">
             <br>
-            <input type="number" id="id" name="_id" placeholder="ID" value="">
+            {#await fetchData}
+                fetching...
+            {:then} 
+            <input type="number" id="id" name="_id" placeholder="ID" value="{largestId}" readonly>
 
+            {/await}
             <input type="submit" value="Submit">
-
+            
         </form>
     </div>
   </div>
