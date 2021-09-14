5343daef614d59b0e435ef13ef329fc0c01e1845
Bailey Heyworth <bailey@baileyslaptop.lan>
2021-09-14 21:16:42 +0100

    added a bash script to stop the mongo service then start it again as it gives me errors and this is my simple fix

--- backend/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#mongod stays running all the time for me and I have to restart it then run it otherwise it cries at me and i dont know why, so i use this as a simple fix
-sudo systemctl stop mongodb.service
-sudo mongod & node ./server.js
