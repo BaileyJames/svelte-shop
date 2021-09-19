54939c43c2abe3e201755b798f903740f303cfb0
BaileyJames <bailey@baileyslaptop.lan>
2021-09-19 23:32:47 +0100

    added some styles to make the site a little more bearable to look at

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,39 +1,18 @@
-<div class="wrapper">
-    <h1>Cool shop</h1>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/products">Products</a></li>
-        <li><a href="/contact">Contact</a></li>
-    </ul>
-</div>
-<style>
-    * {
-        margin: 0;
-        padding: 0;
-    }
-    .wrapper {
-        position: relative;
-        display: grid;
-        width: 90%;
-        grid-template-columns: 25vw auto;
-        margin: auto;
-    }
-    .wrapper ul {
-        position: absolute;
-        right: 2em;
-        height: 100%;
-    }
-    .wrapper ul li {
-        position: relative;
-        list-style-type: none;
-        display: inline-block;
-        top: 1em;
-    }
-    .wrapper ul li a {
-        text-decoration: none;
-        display: block;
-        padding: 0 2em;
+<script>
+    import style from "../styles/style.css"
+</script>
+<header>
+    <div class="container">
+        <nav class="nav flex">
+            <h1>Cool shop company</h1>
+            <ul class="flex">
+                <li class="nav__item"><a href="/">Home</a></li>
+                <li class="nav__item"><a href="/products">Products</a></li>
+                <li class="nav__item"><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
 
-    }
-</style>
+
 <slot></slot>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,7 +1,13 @@
-<p>Hello</p>
-<p>This is just a "simple" shop idea I made with <a href="https://svelte.dev/">Svelte</a>,
+
+<head>
+    <link rel="stylesheet" type="text/css" href="./style.css"> 
+</head>
+<div class="container">
+    <p>Hello</p>
+    <p>This is just a "simple" shop idea I made with <a href="https://svelte.dev/">Svelte</a>,
 or more specifically, <a href="https://kit.svelte.dev/">SvelteKit.</a></p>
-<p>All this essentially is, is a basic shop connected to a database of products that
+    <p>All this essentially is, is a basic shop connected to a database of products that
     automatically updates whenever a new product is added. The product is added to /products
     and when you click on the product, it takes you to an automatically generated page for it.
 </p>
+</div>
